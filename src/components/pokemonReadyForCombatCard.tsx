2d62c0fe5516936d4d75c67e350a80acd1c1cfd2
Johan <joenmodu99@gmail.com>
2023-11-24 13:59:59 -0400

    fix: error un detail page load fixed and refactor in pokemon actions

--- src/components/pokemonReadyForCombatCard.tsx
+++ src/components/pokemonReadyForCombatCard.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Result } from '../types/types'
 import { usePokemonData } from '../hooks/usePokemonData'
 
-export const PokemonReadyForCombarCard = ({ name = '', url = '' }: Result) => {
+export const PokemonReadyForCombatCard = ({ name = '', url = '' }: Result) => {
   const { data, removePokemonReadyForCombat } = usePokemonData(url)
 
   return (
     <article className='flex flex-col justify-center items-center  p-2  relative  '>
       <span
         className='absolute top-0 right-1 cursor-pointer z-50'
-        onClick={() => removePokemonReadyForCombat(name)}
+        onClick={() => removePokemonReadyForCombat({ name, url })}
       >
         🗑️
       </span>
